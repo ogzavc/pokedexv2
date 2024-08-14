587aa1e1d2af15c4a5898fcd5fe05580040c5ad6
Oğuz Avcı <ogzavc@gmail.com>
2024-08-14 10:38:15 +0300

    no pokemons & loading states handled on my pokemons page

--- src/app/my-pokemons/page.js
+++ src/app/my-pokemons/page.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
+import Skeleton from "@mui/material/Skeleton";
 import { fetchPokemons } from "@/lib/features/pokemonsSlice/pokemonSlice";
 import { fetchPokemonDetails } from "@/lib/features/pokemonDetailsSlice/pokemonDetailsSlice";
 import { myFavoritePokemons } from "@/utils/helpers";
@@ -23,6 +24,8 @@ export default function MyPokemons() {
       Promise.all(
         pokemons.map((name) => dispatch(fetchPokemonDetails(name)))
       ).then(() => setIsLoading(false));
+    } else {
+      setIsLoading(false);
     }
   }, [pokemons, dispatch]);
 
@@ -30,18 +33,42 @@ export default function MyPokemons() {
     router.push(`/details/${pokemonName}`);
   };
 
+  if (isLoading) {
+    return (
+      <main className={styles.main}>
+        <div className={styles.homeWrapper}>
+          {Array.from({ length: 12 }).map((_, index) => (
+            <Skeleton
+              key={index}
+              variant="rounded"
+              animation="wave"
+              width={340}
+              height={150}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.homeWrapper}>
-        {!isLoading &&
-          pokemons.map((name) => (
+      {pokemons.length > 0 ? (
+        <div className={styles.homeWrapper}>
+          {pokemons.map((name) => (
             <PokeCard
               key={name}
               pokemonName={name}
               onClick={() => handleCardClick(name)}
             />
           ))}
-      </div>
+        </div>
+      ) : (
+        <span>
+          There are no Pokémon here... Go to the details click the like button
+          on the top right to collect one!
+        </span>
+      )}
     </main>
   );
 }
